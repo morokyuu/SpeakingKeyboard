152a0713ab078f9ca036d84e3906ee8e53d40244
zota <zota@example.com>
2023-05-20 23:41:11 +0900

    font png

--- sample/font_gen.py
+++ sample/font_gen.py
@@ -8,18 +8,17 @@ CHIPSIZE=70
 text = "あいうえおかきくけおさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよわをん"
 print(len(text))
 
-img = Image.new("RGBA",(CHIPSIZE,CHIPSIZE*len(text)),(100,100,100,100))
+img = Image.new("RGBA",(CHIPSIZE,CHIPSIZE*len(text)),(0,0,123,255))
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("meiryo.ttc", 64)
 
 def drawtext(char,y):
     chWidth, chHight = draw.textsize(char, font=font)
-    color = (255, 0, 0, 255)
-    #print(text[0])
-    #print(f"width={chWidth},hight={chHight}")
+    color = (0, 255, 0, 255)
     draw.text((0,y*CHIPSIZE), char, color, font=font)
 
 for i in range(len(text)):
     drawtext(text[i],i)
 
+img.save("kana_font.png")
 img.show()
