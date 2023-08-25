c660893563f9ce08adbb394aa4fa88ab06a53f6b
zota <zota@example.com>
2023-08-26 07:25:16 +0900

    re.sub

--- sample/trans_flow/encode_kana.py
+++ sample/trans_flow/encode_kana.py
@@ -10,6 +10,9 @@ https://grapebanana.com/python-encoding-utf-8-7161/
 
 moji-code table
 https://pentan.info/doc/unicode_list.html
+
+文字コード表に載ってるbinaryをそのまま使うとencode()の”x81K”とか出た場合どうするか問題に当たるので、
+並び順や値の確認だけ使うことにしておき、コードはあくまでencode()で作って、それと入力ラベルを比較するのがいい。
 """
 
 
@@ -17,13 +20,14 @@ import re
 
 
 # spell ='きくますとのは゜てみし゛らたす゛'
-spell = 'は゜'
+spell = 'は゜か゛'
 # spell = 'ぱ'
 code = spell.encode('cp932')
 print(spell)
 print(code)
 
 result = re.sub(b'\x82\xcd\x81K', b'\x82\xcf', code)
+result = re.sub(b'\x82\xa9\x81J', b'\x82\xaa', result)
 
 print(result)
 print(result.decode('cp932'))
