eb66a4b84cf9961fa5066a41ebf5ca2918fd0283
zota <zota@example.com>
2023-09-01 23:56:16 +0900

    mp3 generator

---
+++ wav/kana-words/announce-batch.py
@@ -0,0 +1,18 @@
+#!/bin/python
+# pyenv anaconda3
+#
+# kana-dict.txtを読み込んでmp3を作成する。gttsはオンライン必要。
+# 単語リストはshiftjis-CRLFで作成する。
+from gtts import gTTS
+import sys
+
+with open("../../kana-dict.txt",encoding='shift_jis') as fp:
+    lines = fp.readlines()
+    lines = [line.rstrip() for line in lines]
+
+print(lines)
+
+for msg in lines:
+    output = gTTS(text=msg,lang='ja',slow=False)
+    output.save(f'kana-words/{msg}.mp3')
+
