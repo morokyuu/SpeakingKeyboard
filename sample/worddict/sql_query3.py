88be2d64e7baf8d176722aa06e1033df205366a0
zota <zota@example.com>
2024-07-12 12:32:21 +0900

    sql parameter passing

--- sample/worddict/sql_query3.py
+++ sample/worddict/sql_query3.py
@@ -5,6 +5,8 @@ Created on Thu Jul 11 23:31:39 2024
 @author: square
 
 正規表現で検索
+regexpでやろうとしたらうまくいかず。
+https://stackoverflow.com/questions/5071601/how-do-i-use-regex-in-a-sqlite-query
 """
 
 import sqlite3
@@ -12,6 +14,7 @@ import sqlite3
 dbname = 'tango.db'
 with sqlite3.connect(dbname) as conn:
     cur = conn.cursor()
-    cur.execute("select * from words where word like 'すず%'")
+    # cur.execute("select * from words where word like 'すず%'")
+    cur.execute("select * from words where words.word REGEXP 'すず*'")
     for row in cur:
         print(row)
---
+++ sample/worddict/sql_query4.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 12:22:59 2024
+
+@author: square
+
+
+sqlite3 passing parameter
+https://docs.python.org/3.10/library/sqlite3.html#sqlite3-howtos
+"""
+
+
+import sqlite3
+
+
+targ = ('すず%',)
+
+dbname = 'tango.db'
+with sqlite3.connect(dbname) as conn:
+    cur = conn.cursor()
+    cur.execute("select * from words where word like ?", targ)
+    for row in cur:
+        print(row)
